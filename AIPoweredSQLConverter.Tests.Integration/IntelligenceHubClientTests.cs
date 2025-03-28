24de53d89b35247ed12c768a1710d066958364ed
Jacob-J-Thomas <92639671+Jacob-J-Thomas@users.noreply.github.com>
2025-03-28 14:47:12 -0500

    Removed out of date tests and updated test constructors

--- AIPoweredSQLConverter.Tests.Integration/IntelligenceHubClientTests.cs
+++ AIPoweredSQLConverter.Tests.Integration/IntelligenceHubClientTests.cs
@@ -4,18 +4,22 @@ using AIPoweredSQLConverter.Business;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Moq;
+using AIPoweredSQLConverter.DAL;
+using AIPoweredSQLConverter.Host.Config;
 
 namespace AIPoweredSQLConverter.Tests.Integration
 {
     public class PromptFlowLogicTests
     {
         private readonly Mock<IAIClientWrapper> _mockAIClientWrapper;
+        private readonly Mock<AppDbContext> _appDbContext;
+        private readonly Mock<StripeSettings> _stripeSettings;
         private readonly PromptFlowLogic _promptFlowLogic;
 
         public PromptFlowLogicTests()
         {
             _mockAIClientWrapper = new Mock<IAIClientWrapper>();
-            _promptFlowLogic = new PromptFlowLogic(_mockAIClientWrapper.Object);
+            _promptFlowLogic = new PromptFlowLogic(_mockAIClientWrapper.Object, _appDbContext.Object, _stripeSettings.Object);
         }
     }
 }
